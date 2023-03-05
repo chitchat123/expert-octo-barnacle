929166cef82287dfdd436101363b1eea364492d8
chitchat <vladislav.stolyarenko@gmail.com>
2023-03-05 14:32:33 +0200

    [add] continue work on api docs

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -1,10 +1,15 @@
+`use client`;
 import React from 'react';
 
+import { getDictionary } from '@helpers/dictionary';
+
 import styles from './styles.module.scss';
-const Login = () => {
+
+const Login = async () => {
+  const dictionary = await getDictionary('ua');
   return (
     <div className={styles.container}>
-      <h1>Sing in</h1>
+      <h1>{dictionary['test']}</h1>
     </div>
   );
 };
