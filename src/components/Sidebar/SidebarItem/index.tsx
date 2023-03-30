f4873379d98faa8a832ee2b1a635b1de0362cb3e
Vania <iv.ssh.fqu@gmail.com>
2023-03-31 00:32:49 +0300

    [add] sidebar & sidebarItem

---
+++ src/components/Sidebar/SidebarItem/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const SidebarItem: FC<SidebarItemProps> = ({}) => {
+  return <div></div>;
+};
+
+export { SidebarItem };
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Sidebar: FC<SidebarProps> = ({}) => {
+  return (
+    <div>
+      <div>content</div>
+    </div>
+  );
+};
+
+export { Sidebar };
