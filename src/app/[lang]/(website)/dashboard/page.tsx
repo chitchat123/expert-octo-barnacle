c00b1ab9722735897bec99d4c479e66bb679132c
Vania <iv.ssh.fqu@gmail.com>
2023-04-04 21:25:46 +0300

    [fix] global loader

--- src/app/[lang]/(website)/dashboard/page.tsx
+++ src/app/[lang]/(website)/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode, Suspense } from 'react';
+import React from 'react';
 
 import { cookies } from 'next/headers';
 
-import Loading from '@components/Loader';
+import { ContentCard } from '@components/ContentCard';
 
 import { delay } from '@helpers/delay';
 import { apiCallUser } from '@helpers/getUserInfo';
@@ -10,42 +10,45 @@ import { apiCallUser } from '@helpers/getUserInfo';
 import styles from './styles.module.scss';
 
 interface PageProps {}
+
 export const dynamic = 'force-dynamic';
 const Page: ({}: PageProps) => Promise<JSX.Element> = async ({}) => {
   const cookieStore = cookies();
   const token = cookieStore.get('token');
 
   const { fullName } = await apiCallUser(token);
-  console.info(fullName, 'data');
-  await delay(2000);
+  await delay(1000);
 
   return (
     <div className={styles.container}>
-      <h1>{fullName}</h1>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignContent: 'center',
-          justifyContent: 'space-around',
-        }}>
-        <Suspense fallback={<Loading />}>
-          <TestLoading>test</TestLoading>
-        </Suspense>{' '}
-        <Suspense fallback={<Loading />}>
-          <TestLoading>test</TestLoading>
-        </Suspense>
+      <h1>Вітаємо, {fullName}</h1>
+      <div className={styles.mainContent}>
+        <div className={styles.currentSemester}>
+          <h3 className={styles.title}>Current semester</h3>
+          <div className={styles.subjects}>
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+            <ContentCard />
+          </div>
+        </div>
+        <div className={styles.news}>
+          <ContentCard />
+          <ContentCard />
+        </div>
       </div>
     </div>
   );
 };
 
-const TestLoading: ({
-  children,
-}: {
-  children: ReactNode;
-}) => Promise<JSX.Element> = async ({ children }) => {
-  return await delay(2000).then(() => children);
-};
+// const TestLoading: ({
+//   children,
+// }: {
+//   children: ReactNode;
+// }) => Promise<JSX.Element> = async ({ children }) => {
+//   return await delay(2000).then(() => children);
+// };
 
 export default Page;
