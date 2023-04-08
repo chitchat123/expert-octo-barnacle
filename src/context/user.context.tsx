7540184265d58455670e4622ae1c8bbef7ab502c
Vania <iv.ssh.fqu@gmail.com>
2023-04-08 17:18:35 +0300

    [fix] some fixes

--- src/context/user.context.tsx
+++ src/context/user.context.tsx
@@ -4,6 +4,8 @@ import React, { createContext, useEffect, useMemo, useState } from 'react';
 
 import Cookies from 'js-cookie';
 
+import { parseBody } from '@helpers/parseUserCredentials';
+
 const INITIAL_STATE = {
   user: null,
   loginUser: (login: string, password: string) => {},
@@ -22,7 +24,7 @@ const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
     })
       .then(response => response.json())
       .then(result => setUser(result))
-      .catch(error => console.error(error, 'An error occured'));
+      .catch(error => console.error(error, 'An error occurred'));
   };
 
   useEffect(() => {
@@ -35,19 +37,10 @@ const UserContextProvider = ({ children }: { children: React.ReactNode }) => {
       getUser();
     }
   }, [token]);
-  console.info(user, 'user');
 
   const loginUser = async (login: string, password: string) => {
-    const body = Object.entries({
-      username: login,
-      password: password,
-      grant_type: 'password',
-    })
-      .map(function ([key, value]) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(value);
-      })
-      .join('&');
-
+    const body = parseBody(login, password);
+    //get and save token. set token to cookie
     await fetch('https://api.campus.kpi.ua/oauth/token', {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       method: 'POST',
