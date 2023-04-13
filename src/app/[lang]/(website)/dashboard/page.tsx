8f7f8680c0e9ea00e7a7fd578b0ef72ec0621239
chitchat <vladislav.stolyarenko@gmail.com>
2023-04-13 20:53:45 +0300

    [fix] query for news

--- src/app/[lang]/(website)/dashboard/page.tsx
+++ src/app/[lang]/(website)/dashboard/page.tsx
@@ -2,16 +2,15 @@ import React, { Suspense } from 'react';
 
 import { cookies } from 'next/headers';
 
-import { ContentCard } from '@components/ContentCard';
 import { GridTable } from '@components/GridTable';
 import Loading from '@components/Loader';
+import { RightBoard } from '@components/RightBoard';
 
 import { delay } from '@helpers/delay';
+import { apiCallBoard } from '@helpers/getBoardData';
 import { apiCallUser } from '@helpers/getUserInfo';
 
 import styles from './styles.module.scss';
-import { RightBoard } from '@components/RightBoard';
-import { apiCallBoard } from '@helpers/getBoardData';
 
 interface PageProps {}
 
@@ -19,18 +18,15 @@ export const dynamic = 'force-dynamic';
 const Page: ({}: PageProps) => Promise<JSX.Element> = async ({}) => {
   const cookieStore = cookies();
   const token = cookieStore.get('token');
-  console.log('token', token)
 
   const { fullName } = await apiCallUser(token);
-  const { data } = await apiCallBoard(token,1,4);
-
-
+  const { data } = await apiCallBoard(token?.value || '', 1, 4);
 
   await delay(1000);
 
   return (
     <div className={styles.dashboardContainer}>
-      {/*<h1>Вітаємо, {fullName}</h1>*/}
+      <h1>Вітаємо, {fullName}</h1>
       <div className={styles.mainContent}>
         <div className={styles.currentSemester}>
           <h3 className={styles.title}>Current semester</h3>
@@ -40,7 +36,7 @@ const Page: ({}: PageProps) => Promise<JSX.Element> = async ({}) => {
           </Suspense>
         </div>
         <div className={styles.news}>
-          <RightBoard news={data}/>
+          <RightBoard news={data} />
         </div>
       </div>
     </div>
--- src/helpers/getBoardData.ts
+++ src/helpers/getBoardData.ts
@@ -1,18 +1,24 @@
-export const apiCallBoard = async (token: any, page: number, size: number) => {
-    return await fetch(`https://api.campus.kpi.ua/Board/All?page=${page}&size=${size}`, {
+export const apiCallBoard = async (
+  token: string,
+  page: number,
+  size: number
+) => {
+  console.info(token);
+  return await fetch(
+    `https://api.campus.kpi.ua/Board/All?page=${page}&size=${size}`,
+    {
       method: 'GET',
       headers: {
         authorization: `Bearer ${token}`,
-      }
+      },
+    }
+  )
+    .then(res => res.json())
+    .then(data => {
+      console.info('data', data);
+      return data;
     })
-      .then(res => res.json())
-      .then(data =>{
-        console.log('data',data)
-        return data;
-        })
-      .catch(err => {
-        throw new Error('could not get board data', err);
-      });
-  };
-  
-  
+    .catch(err => {
+      throw new Error('could not get board data', err);
+    });
+};
